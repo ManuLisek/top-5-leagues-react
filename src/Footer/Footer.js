ede67a003f0f2164efeeb434b019fdc867301f1c
Michał Lisowiec <miclisowiec@gmail.com>
2021-08-23 20:07:52 +0200

    Add component Footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => (
+  <footer className="footer">
+    <p className="footer__description">&copy; TOP 5 LEAGUES. All rights reserved.</p>
+    <p className="footer__description">Football data provided by the Football-Data.org API and Scorebat.com</p>
+  </footer>
+);
+
+export default Footer; 
--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from './Navigation/Navigation';
+import Footer from './Footer/Footer';
 import PropTypes from 'prop-types';
 
 const MainLayout = ({children}) => (
   <div className="main-layout">
     <Navigation />
     {children}
+    <Footer />
   </div>
 );
 

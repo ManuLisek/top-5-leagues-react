b9889fdda609930f1f5a4f37f44dd5c98deeae07
Michał Lisowiec <miclisowiec@gmail.com>
2021-08-23 19:46:33 +0200

    Add component NotFound and Redirect if url is incorrect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import MainLayout from './MainLayout';
 import Home from './Home';
 import PremierLeague from './Leagues/PremierLeague';
@@ -7,6 +7,7 @@ import PrimeraDivision from './Leagues/PrimeraDivision';
 import Bundesliga from './Leagues/Bundesliga';
 import SerieA from './Leagues/SerieA';
 import Ligue1 from './Leagues/Ligue1';
+import NotFound from './NotFound/NotFound';
 import * as HighlightsAPI from './api/HighlightsAPI';
 import './styles/global.scss';
 
@@ -73,6 +74,11 @@ const App = () => {
                 highlights={highlights} 
               />
             }
+          />
+          <Route 
+            render={() => 
+              <Redirect to={{pathname: '/not-found'}} />} 
+            component={NotFound}
           />
         </Switch>
       </MainLayout>
---
+++ src/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.scss';
+
+function NotFound(){
+  return(
+    <>
+      <div className='not-found'>Nie ma takiej strony.</div>
+    </>
+  );
+}
+
+export default NotFound;

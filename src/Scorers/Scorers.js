ec879aceda4fed4955b11ede1abb38d373995a52
Michał Lisowiec <miclisowiec@gmail.com>
2021-09-15 15:36:17 +0200

    Handle errors for standings and scorers

--- src/Scorers/Scorers.js
+++ src/Scorers/Scorers.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Error from '../Error/Error';
 import './Scorers.scss';
 
 
 const Scorers = (props) => {
-  const {scorers} = props;
+  const {scorers, haveScorersError} = props;
   const scorersList = scorers.map((scorer, i) => (
     <tr key={scorer.player.id}>
       <td className="scorers__td scorers__td--bold">{i + 1}</td>
@@ -17,22 +18,26 @@ const Scorers = (props) => {
   ));
 
   return(
-    <table className="scorers">
-      <thead className="scorers__thead">
-        <tr className="scorers__tr">
-          <th className="scorers__th">#</th>
-          <th className="scorers__th" colSpan="2">Player</th>
-          <th className="scorers__th">Goals</th>
-        </tr>
-      </thead>
-      <tbody className="scorers__tbody">
-        {scorersList}
-      </tbody>
-    </table>
+    !haveScorersError
+      ?   <table className="scorers">
+        <thead className="scorers__thead">
+          <tr className="scorers__tr">
+            <th className="scorers__th">#</th>
+            <th className="scorers__th" colSpan="2">Player</th>
+            <th className="scorers__th">Goals</th>
+          </tr>
+        </thead>
+        <tbody className="scorers__tbody">
+          {scorersList}
+        </tbody>
+      </table>
+      :   <Error />
+
   );};
 
 Scorers.propTypes = {
   scorers: PropTypes.array,
+  haveScorersError: PropTypes.bool,
 };
 
 
--- src/Standings/Standings.js
+++ src/Standings/Standings.js
@@ -3,31 +3,34 @@ import PropTypes from 'prop-types';
 import './Standings.scss';
 
 const Standings = (props) => {
-  const {table, tableKey} = props;
+  const {haveStandingsError, table, tableKey} = props;
 
   return(
-    <table className="standings">
-      <thead className="standings__thead">
-        <tr className="standings__tr">
-          <th className="standings__th">#</th>
-          <th className="standings__th" colSpan="2">Club</th>
-          <th className="standings__th">Pl</th>
-          <th className="standings__th">W</th>
-          <th className="standings__th">D</th>
-          <th className="standings__th">L</th>
-          <th className="standings__th">+/-</th>
-          <th className="standings__th">Pts</th>
-        </tr>
-      </thead>
-      <tbody className="standings__tbody">
-        {table}
-        {tableKey}
-      </tbody>
-    </table>
+    !haveStandingsError
+      ?   <table className="standings">
+        <thead className="standings__thead">
+          <tr className="standings__tr">
+            <th className="standings__th">#</th>
+            <th className="standings__th" colSpan="2">Club</th>
+            <th className="standings__th">Pl</th>
+            <th className="standings__th">W</th>
+            <th className="standings__th">D</th>
+            <th className="standings__th">L</th>
+            <th className="standings__th">+/-</th>
+            <th className="standings__th">Pts</th>
+          </tr>
+        </thead>
+        <tbody className="standings__tbody">
+          {table}
+          {tableKey}
+        </tbody>
+      </table>
+      :  ''
   );
 };
 
 Standings.propTypes = {
+  haveStandingsError: PropTypes.bool,
   table: PropTypes.array,
   tableKey: PropTypes.object,
 };

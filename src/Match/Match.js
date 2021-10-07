c499b122ab940876bb7bcf808049ae26a339fbc8
Michał Lisowiec <miclisowiec@gmail.com>
2021-10-07 21:05:26 +0200

    Reload video if hidden

--- src/Match/Match.js
+++ src/Match/Match.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import PropTypes from 'prop-types';
 import './Match.scss';
@@ -9,8 +9,16 @@ function Match(props) {
   const host = match.side1.name;
   const guest = match.side2.name;
   const [isMatchActive, setIsMatchActive] = useState(false);
+  const video = useRef(null);
   const handleToggleShowVideo = () => {
     setIsMatchActive(prevState => !prevState);
+    const iframe = video.current.querySelector('iframe._scorebatEmbeddedPlayer_');
+    if ( isMatchActive ) {
+      setTimeout(function reloadIframe(){
+        const iframeSrc = iframe.src;
+        iframe.src = iframeSrc;
+      }, 500);
+    }
   };
 
   return (
@@ -24,7 +32,7 @@ function Match(props) {
             <i className={`fas fa-arrow-alt-circle-down match__arrow ${isMatchActive ? 'match__arrow--active' : ''}`}></i>
           </div>
         </div>
-        <div className={`match__video ${isMatchActive ? 'match__video--active' : ''}`}>
+        <div className={`match__video ${isMatchActive ? 'match__video--active' : ''}`} ref={video}>
           {ReactHtmlParser(`${match.embed}`)}
         </div>
       </div>

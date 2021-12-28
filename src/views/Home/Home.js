d6ac113cdd00cb53a2cb65ad4048d6f3385ca7a2
Michał Lisowiec <miclisowiec@gmail.com>
2021-12-28 20:21:49 +0100

    Add content to the Home page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,7 +4,8 @@ import './Home.scss';
 const Home = () => {
   return (
     <div className="home">
-      <h1 className="home__header">Home Page</h1>
+      <h1 className="home__header">Top 5 Leagues</h1>
+      <p className="home__content">Welcome to the Top 5 Leagues. This page was created because I am learning programming and this is one of my projects. You can find here the current league tables, top scorers and highlights of the latest matches in the top 5 European leagues. The data comes from two different APIs.</p>
     </div>
   );
 };
